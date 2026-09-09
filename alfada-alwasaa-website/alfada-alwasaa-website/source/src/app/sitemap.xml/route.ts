b587e7a3b8d85c508e8edd60ee775f39d8ab88aa
Al-Fadaa Developer <dev@alfadaalwasaa.com>
2026-09-09 09:16:42 +0300

    إصلاح مسارات sitemap.xml و robots.txt لتوليدها رسمياً عبر Next.js Route Handlers

---
+++ alfada-alwasaa-website/alfada-alwasaa-website/source/src/app/sitemap.xml/route.ts
@@ -0,0 +1,42 @@
+export async function GET() {
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.alfadaalwasaa.com/</loc>
+    <lastmod>2026-09-09</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.alfadaalwasaa.com/#about</loc>
+    <lastmod>2026-09-09</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.alfadaalwasaa.com/#services</loc>
+    <lastmod>2026-09-09</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.alfadaalwasaa.com/#sectors</loc>
+    <lastmod>2026-09-09</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.alfadaalwasaa.com/#contact</loc>
+    <lastmod>2026-09-09</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>`;
+
+  return new Response(sitemapXml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=86400, stale-while-revalidate=43200",
+    },
+  });
+}
