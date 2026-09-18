fb83b37be441f37ab90ee3db4e0a54d4b26d8d18
DrMuhammed11 <mo1mo7hammed@gmail.com>
2026-09-18 08:50:36 +0300

    fix(website): تحديث route الـ sitemap.xml وإزالة روابط الهاش

--- alfada-alwasaa-website/alfada-alwasaa-website/source/src/app/sitemap.xml/route.ts
+++ alfada-alwasaa-website/alfada-alwasaa-website/source/src/app/sitemap.xml/route.ts
@@ -3,33 +3,9 @@ export async function GET() {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://www.alfadaalwasaa.com/</loc>
-    <lastmod>2026-09-09</lastmod>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://www.alfadaalwasaa.com/#about</loc>
-    <lastmod>2026-09-09</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>https://www.alfadaalwasaa.com/#services</loc>
-    <lastmod>2026-09-09</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>https://www.alfadaalwasaa.com/#sectors</loc>
-    <lastmod>2026-09-09</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://www.alfadaalwasaa.com/#contact</loc>
-    <lastmod>2026-09-09</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
   </url>
 </urlset>`;
 
